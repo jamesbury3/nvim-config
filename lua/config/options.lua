9adc4ef562227faf4cbd6b05f7330b87f99637d3
James Bury <jamesbury77@gmail.com>
2025-04-08 11:25:31 -0400

    feat: adds tab config to options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,9 @@
 
 -- On ubuntu, this requires a clipboard tool. xclip works, so install with `sudo apt install xclip`
 vim.opt.clipboard = "unnamedplus"
+
+vim.g.autoformat = false -- prevents autoformatting when a file is saved
+vim.o.tabstop = 4 -- tab looks like 4 spaces
+vim.o.expandtab = true -- use spaces instead of tabs
+vim.o.softtabstop = 4 -- number of spaces to use instead of tab
+vim.o.shiftwidth = 4 -- number of spaces inserted when indenti.o
