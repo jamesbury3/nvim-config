9bf0ebcce4f346b1867afb6ba59a6bc5a85e68be
James Bury <jamesbury77@gmail.com>
2025-04-07 22:22:21 -0400

    fix: copy/paste work using xclip

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- On ubuntu, this requires a clipboard tool. xclip works, so install with `sudo apt install xclip`
+vim.opt.clipboard = "unnamedplus"
