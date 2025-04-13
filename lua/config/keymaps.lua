98283ee1998a52a0404cc394acbfdb5546d7d6ec
James Bury <jamesbury77@gmail.com>
2025-04-13 09:12:07 -0400

    feat: adds custom formatter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,25 @@
 local map = LazyVim.safe_keymap_set
 
 map("n", "<leader>jq", ":%!jq --indent 4 '.'<CR>", { desc = "format json with jq" })
+map('n', '<leader>cf', ':lua Format_file()<CR>', { noremap = true, silent = true })
+
+-- Define the function
+function Format_file()
+  local filetype = vim.bo.filetype -- Get the current filetype
+  if filetype == "yaml" then
+    print("formatting yaml...")
+    local filepath = vim.api.nvim_buf_get_name(0)
+    vim.fn.system('npx prettier --write ' .. filepath)
+    vim.cmd("e!")
+  elseif filetype == "python" then
+    print("formatting python...")
+    local filepath = vim.api.nvim_buf_get_name(0)
+    vim.fn.system('source ~/.config/nvim/formatters/venv/bin/activate')
+    vim.fn.system('black ' .. filepath)
+    vim.cmd("silent! !e")
+    vim.fn.system("deactivate")
+  else
+    print("No formatter configured for this filetype: " .. filetype)
+  end
+end
+
