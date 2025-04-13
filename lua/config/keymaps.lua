62248f30e181004d58278873bfc87f7b870d5ebe
James Bury <jamesbury77@gmail.com>
2025-04-13 09:41:10 -0400

    feat: adds json formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,14 +7,18 @@ local map = LazyVim.safe_keymap_set
 map("n", "<leader>jq", ":%!jq --indent 4 '.'<CR>", { desc = "format json with jq" })
 map('n', '<leader>cf', ':lua Format_file()<CR>', { desc = "format file with custom formatter", noremap = true, silent = true })
 
--- Define the function
-function Format_file()
-  local filetype = vim.bo.filetype -- Get the current filetype
-  if filetype == "yaml" then
-    print("formatting yaml...")
+local function format_with_prettier()
+    print("formatting with prettier...")
     local filepath = vim.api.nvim_buf_get_name(0)
     vim.fn.system('npx prettier --write ' .. filepath)
     vim.cmd("silent! !e")
+end
+
+-- Define the function
+function Format_file()
+  local filetype = vim.bo.filetype -- Get the current filetype
+  if filetype == "yaml" or filetype == "json" then
+    format_with_prettier()
   elseif filetype == "python" then
     print("formatting python...")
     local filepath = vim.api.nvim_buf_get_name(0)
