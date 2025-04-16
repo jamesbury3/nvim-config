3a7128706dea0e2158e4cae4ccfb3e39d8c75fa5
James Bury <jamesbury77@gmail.com>
2025-04-15 21:53:04 -0400

    refac: moves formatter code to new file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,55 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local formatter = require("plugins.custom.formatter")
 
 local map = LazyVim.safe_keymap_set
 
-map(
-  "n",
-  "<leader>cf",
-  ":lua Format_file()<CR>",
-  { desc = "format file with custom formatter", noremap = true, silent = true }
-)
-
-local function reload_file()
-  vim.cmd("silent! !e")
-end
-
-local function format_with_prettier()
-  local filepath = vim.api.nvim_buf_get_name(0)
-  vim.fn.system("npx prettier --write " .. filepath)
-  reload_file()
-end
-
-function Format_file()
-  local filetype = vim.bo.filetype
-  if filetype == "yaml" then
-    print("formatting yaml...")
-    format_with_prettier()
-  elseif filetype == "json" then
-    print("formatting json...")
-    format_with_prettier()
-  elseif filetype == "java" then
-    print("formatting java...")
-    format_with_prettier()
-  elseif filetype == "python" then
-    print("formatting python...")
-    local filepath = vim.api.nvim_buf_get_name(0)
-    vim.fn.system("source ~/.config/nvim/formatters/python/venv/bin/activate")
-    vim.fn.system("black " .. filepath)
-    vim.fn.system("deactivate")
-    reload_file()
-  elseif filetype == "lua" then
-    print("formatting lua...")
-    local filepath = vim.api.nvim_buf_get_name(0)
-    vim.fn.system("npx stylua " .. filepath)
-    reload_file()
-  elseif filetype == "go" then
-    print("formatting go...")
-    local filepath = vim.api.nvim_buf_get_name(0)
-    vim.fn.system("gofmt -w " .. filepath)
-    reload_file()
-  else
-    print("No formatter configured for this filetype: " .. filetype)
-  end
-end
+map("n", "<leader>cf", function()
+  formatter.format_file()
+end, { desc = "format file with custom formatter", noremap = true, silent = true })
