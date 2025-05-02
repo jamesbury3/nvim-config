7c6d5ea87ec8759772cbcbc7a385fba5fd27c1d1
James Bury <jamesbury77@gmail.com>
2025-05-02 18:58:21 -0400

    feat: sets telescope keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
