5793e9ab890f5483bec878a014719248a66a181e
James Bury <jamesbury77@gmail.com>
2025-05-03 15:07:07 -0400

    feat: telescope ignores git directory

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
 return {
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8', }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		opts = {
+			defaults = {
+				file_ignore_patterns = {
+					"^.git/",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		},
+	},
 }
