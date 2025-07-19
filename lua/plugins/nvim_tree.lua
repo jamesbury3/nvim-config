e08ac17c7e014a41c52ef5e8936cb7a48d2b566c
James Bury <jamesbury77@gmail.com>
2025-07-19 10:10:07 -0400

    feat: nvim-tree closes after selecting a file

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -6,7 +6,28 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			api.config.mappings.default_on_attach(bufnr)
+
+			vim.keymap.set("n", "<CR>", function()
+				local node = api.tree.get_node_under_cursor()
+				if node and not node.nodes then -- if it's not a directory
+					api.node.open.edit()
+					api.tree.close()
+				else
+					api.node.open.edit() -- just expand/collapse directory
+				end
+			end, opts("Open and maybe Close Tree"))
+		end
+
 		require("nvim-tree").setup({
+			on_attach = my_on_attach,
 			git = {
 				enable = true,
 				ignore = false,
