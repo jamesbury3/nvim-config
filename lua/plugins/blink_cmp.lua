c1e5d8bfe2d0a1b2b9876db1aa7aea65255f7474
James Bury <jamesbury77@gmail.com>
2025-08-24 14:53:51 -0400

    feat: blink shows documentation

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -13,13 +13,14 @@ return {
 		},
 		completion = {
 			documentation = {
-				auto_show = false,
+				auto_show = true,
 			},
 		},
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 		fuzzy = { implementation = "prefer_rust_with_warning" },
+		signature = { enabled = true },
 	},
 	opts_extend = { "sources.default" },
 }
