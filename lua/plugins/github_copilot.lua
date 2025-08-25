37dab148b186a1f4a130486f46ce1ff90bf96cad
James Bury <jamesbury77@gmail.com>
2025-08-25 07:24:17 -0400

    fix: updates copilot keymaps

--- lua/plugins/github_copilot.lua
+++ lua/plugins/github_copilot.lua
@@ -1,4 +1,37 @@
 return {
 	"github/copilot.vim",
 	cmd = "Copilot",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set(
+			"i",
+			"<C-j>",
+			'copilot#Accept("")',
+			{ expr = true, replace_keycodes = false, desc = "Accept copilot suggestion" }
+		)
+		vim.keymap.set(
+			"i",
+			"<C-l>",
+			"copilot#AcceptWord()",
+			{ expr = true, replace_keycodes = false, desc = "Accept next word" }
+		)
+		vim.keymap.set(
+			"i",
+			"<C-;>",
+			"copilot#Next()",
+			{ expr = true, replace_keycodes = false, desc = "Next suggestion" }
+		)
+		vim.keymap.set(
+			"i",
+			"<C-,>",
+			"copilot#Previous()",
+			{ expr = true, replace_keycodes = false, desc = "Previous suggestion" }
+		)
+		vim.keymap.set(
+			"i",
+			"<C-i>",
+			"copilot#Dismiss()",
+			{ expr = true, replace_keycodes = false, desc = "Dismiss suggestion" }
+		)
+	end,
 }
